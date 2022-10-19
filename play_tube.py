95c838f7f09fabfc1dff9b8bf248ddd48b2fe979
WelderMileu <45273258+WelderMileu@users.noreply.github.com>
2022-10-19 19:09:35 -0300

    Update play_tube.py
    update file

--- play_tube.py
+++ play_tube.py
@@ -1,35 +1,47 @@
 #!/usr/bin/python3
-from pytube import YouTube
+import cv2
 import sys
+from pytube import YouTube
 import os
-from time import sleep
 
-def remove_v():
-    video_path = os.listdir()
-    
-    for x in video_path:
-        if x.__contains__(".mp4"):
-            os.remove(x)
-    
-    sleep(.5)
-
-def download_v(share):
+def download_video(domain):
     try:
-        yt   = YouTube(str(share))
-        yt_s = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
-        
-        sleep(5)
-        
-        start_video = 'parole --fullscreen "{}"'.format(yt_s)
-        os.system(start_video)
+        yt = YouTube(str(domain))
+        download = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+    
+        os.rename(download, 'video.mp4')
 
     except Exception as err:
         print(err)
 
-def main(share):
-    download_v(share)
-    remove_v()
+def remove_video():
+    if os.path.exists('video.mp4'):
+        os.remove('video.mp4')
+
+def play_video():
+    try:
+        cap = cv2.VideoCapture("video.mp4")
+        ret, frame = cap.read()
+
+        while (1):
+            ret, frame = cap.read()
+            cv2.imshow('frame', frame)
+            cv2.moveWindow('frame', 40,70)
+
+            if cv2.waitKey(1) & 0xFF == ord('q') or ret==False:
+                cap.release()
+                cv2.destroyAllWindows()
+                break
+
+            cv2.imshow('frame', frame)
+            
+    except Exception as err:
+        print(err)
+
+def main():
+    download_video(sys.argv[1])
+    play_video()
+    remove_video()
 
 if __name__ == '__main__':
-    share = sys.argv[1]
-    main(share)
+    main()
