7f4d62df16128dfe51ffa5bbcd1d0d25b22832f9
WelderMileu <45273258+WelderMileu@users.noreply.github.com>
2022-10-20 16:55:27 -0300

    Update play_tube.py
    format code and add title in video

--- play_tube.py
+++ play_tube.py
@@ -1,47 +1,80 @@
 #!/usr/bin/python3
-import cv2
-import sys
-from pytube import YouTube
-import os
+try:
+    import cv2
+    import sys
+    from pytube import YouTube
+    import os
 
-def download_video(domain):
-    try:
-        yt = YouTube(str(domain))
-        download = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
-    
-        os.rename(download, 'video.mp4')
+except Exception as err:
+    print(err)
 
-    except Exception as err:
-        print(err)
+class Simple:
+    def __init__(self, domain):
+        try:
+            self.domain    = str(domain)
+            self.video_t   = ''
+            self.video_tmp = 'video.mp4'
+            self.video_ext = ''
 
-def remove_video():
-    if os.path.exists('video.mp4'):
-        os.remove('video.mp4')
+        except Exception as err:
+            print(err)
 
-def play_video():
-    try:
-        cap = cv2.VideoCapture("video.mp4")
-        ret, frame = cap.read()
-
-        while (1):
-            ret, frame = cap.read()
-            cv2.imshow('frame', frame)
-            cv2.moveWindow('frame', 40,70)
-
-            if cv2.waitKey(1) & 0xFF == ord('q') or ret==False:
-                cap.release()
-                cv2.destroyAllWindows()
-                break
-
-            cv2.imshow('frame', frame)
+    def download_video(self):
+        def execute():
+            yt = YouTube(self.domain)
+            download = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
             
-    except Exception as err:
-        print(err)
+            self.video_t = yt.title
+            self.video_ext = download
 
-def main():
-    download_video(sys.argv[1])
-    play_video()
-    remove_video()
+            os.rename(download, self.video_tmp)
+
+        def reset():
+            if os.path.exists(self.video_ext):
+                os.remove(self.video_ext)
+
+            if os.path.exists(self.video_tmp):
+                os.remove(self.video_tmp)
+
+        try:
+            execute()
+
+        except Exception as err:
+            print(err)
+            reset()
+            execute()
+
+    def remove_video(self):
+        if os.path.exists(self.video_tmp):
+            os.remove(self.video_tmp)
+
+    def play_video(self):
+        try:
+            cap = cv2.VideoCapture(self.video_tmp)
+            ret, frame = cap.read()
+
+            ts = self.video_t.encode('utf-8') 
+
+            while (1):
+                ret, frame = cap.read()
+                cv2.imshow(str(ts), frame)
+                cv2.moveWindow(str(ts), 40,70)
+
+                if cv2.waitKey(1) & 0xFF == ord('q') or ret==False:
+                    cap.release()
+                    cv2.destroyAllWindows()
+                    break
+
+                cv2.imshow(str(ts), frame)
+            
+        except Exception as err:
+            print(err)
+
+    def main(self):
+        Simple.download_video()
+        Simple.play_video()
+        Simple.remove_video()
 
 if __name__ == '__main__':
-    main()
+    Simple = Simple(sys.argv[1])
+    Simple.main()
