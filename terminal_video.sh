43fc571f08e24598108d540699cdf683ffe41f38
WelderMileu <45273258+WelderMileu@users.noreply.github.com>
2022-11-22 14:24:09 -0300

    Update terminal_video.sh

--- terminal_video.sh
+++ terminal_video.sh
@@ -3,19 +3,26 @@
 loop=True
 
 while [ $loop ]; do
-	read -p "[ prompt-video ] -> $ " param
+	echo -en "\e[0;36m ▶️ \e[0m\e[0;36m[\e[0mprompt-video\e[0;36m]$\e[0m "
+	read -p '' param
 
-	if [ -z $param ]; then
-	    echo "[s] search + <search_str>";
-		echo "[v] tabs of the videos";
-		echo "[c] coby url domain";
-		echo "[p] play video + <url>";
-		echo "[clc] clear cache in browser"
-		echo "[cls] clear terminal"
-		echo "[q] quit";
+	if [ -z $param  ]; then
+		echo -n "";
+
+	elif [ $param = "-h" ]; then
+	    echo "";
+		echo "[-h]  - menu help"
+	    echo "[s]   - consult search";
+		echo "[v]   - tabs of the videos";
+		echo "[c]   - coby url domain";
+		echo "[p]   - play video";
+		echo "[clc] - clear cache in browser"
+		echo "[cls] - clear terminal"
+		echo "[q]   - quit";
+		echo "";
 
 	elif [ $param = 's' ]; then
-		read -p 'param search : ' search
+		read -p '[param search] => ' search
 		python tools.py s "$search"
 
 	elif [ $param = 'v' ]; then
@@ -28,7 +35,7 @@ while [ $loop ]; do
 		python tools.py clc
 
 	elif [ $param = 'p' ]; then
-		read -p 'url video: ' video
+		read -p '[domain video] => ' video
 		python play.py "$video"
 	
 	elif [ $param = 'cls' ]; then
